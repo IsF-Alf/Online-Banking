bc63f97e4a610242f02dd217fc86bbda25db23f4
Isaac <ifa24991@gmail.com>
2024-02-13 20:52:46 -0300

    modificaciones de seguridad

---
+++ src/main/java/com/isd/Homebanking/configurations/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.isd.Homebanking.configurations;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(ant-> ant
+                        .requestMatchers("/", "/index.html" , "/web/assets/images/**" , "/web/assets/pages/login.html" ,
+                                "/web/assets/js/index.js" , "/web/assets/js/login.js" , "/web/assets/style/style.css").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/clients" , "/api/login").permitAll()
+                        .requestMatchers("/administrator/**" , "/h2-console/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/rest/**" , "/api/admin/loans").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/clients").hasAuthority("ADMIN")
+                        .requestMatchers("/web/**").hasAuthority("CLIENT")
+                        .requestMatchers(HttpMethod.POST, "/api/clients/current/accounts" , "/api/clients/current/cards", "/api/clients/current/export-pdf" , "/api/clients/current/transfers" , "/api/loans" , "/api/loans/payments").hasAuthority("CLIENT")
+                        .requestMatchers(HttpMethod.GET, "/api/clients/current" , "/api/accounts/{id}" , "/api/clients/current/accounts" , "/api/loans").hasAuthority("CLIENT")
+                        .requestMatchers(HttpMethod.PATCH, "/api/clients/current/cards" , "/api/clients/current/accounts").hasAuthority("CLIENT")
+                        .anyRequest().denyAll())
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .headers(header -> header.frameOptions((frameOptionsConfig -> frameOptionsConfig.disable())))
+                .formLogin(formLogin -> formLogin.loginPage("/login.html")
+                        .loginProcessingUrl("/api/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .successHandler((request, response, authentication) -> clearAuthenticationAttributes(request))
+                        .failureHandler((request, response, exception) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutUrl("/api/logout")
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+                        .deleteCookies("JSESSIONID"))
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, exception) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+                .rememberMe(Customizer.withDefaults());
+        return http.build();
+    }
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+
+        }
+
+    }
+}
--- src/main/java/com/isd/Homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/isd/Homebanking/configurations/WebAuthentication.java
@@ -1,10 +1,15 @@
 package com.isd.Homebanking.configurations;
 
+import com.isd.Homebanking.models.Client;
 import com.isd.Homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
@@ -14,5 +19,22 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inputEmail -> {
+            Client client = clientRepository.findByEmail(inputEmail);
+            if (client != null) {
+                if (client.getEmail().contains("mindbank")) {
+                    return new User(client.getEmail(), client.getPassword(),
+                            AuthorityUtils.commaSeparatedStringToAuthorityList("ADMIN,CLIENT"));
+                } else {
+                    return new User(client.getEmail(), client.getPassword(),
+                            AuthorityUtils.createAuthorityList("CLIENT"));
+                }
+            } else {
+                throw new UsernameNotFoundException("Unknown client" + inputEmail);
+            }
+        });
     }
 }
